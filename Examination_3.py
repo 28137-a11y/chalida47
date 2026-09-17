37b101d6b4a0d5a8746afb90b828e33103be4aff
chalida47 <28137@ud.ac.th>
2026-09-17 10:12:46 +0700

    Update Examination_3.py

--- Examination_3.py
+++ Examination_3.py
@@ -6,5 +6,7 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-price = float(input())
-is_member = int(input())
+price = float(input("ราคาสินค้าเต็ม:"))
+is_member = int(input("สถานะสมาชิก: "))
+if เป็นสมาชิก = 1
+print= (ลดทันที 10)
